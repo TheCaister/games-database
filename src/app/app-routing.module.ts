ddaa5aa154d6d97ece8faa37e2d50eca77dca96b
Edison Cai <edisoncai2001@gmail.com>
2021-07-28 15:00:21 +0100

    Adding home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+// Defining some routes
+const routes: Routes = [
+  // For each route, the path is the URL and the component is what the URL points to
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    // Branch off to different games
+    path: 'search/:game-search',
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
