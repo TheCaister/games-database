a2436206362d84b9d66b239151bfe58379fb790d
Edison Cai <edisoncai2001@gmail.com>
2021-07-31 12:29:19 +0100

    Adding svg for every platform and using the API to load a list of games with their respective info

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from '../../models';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort: string;
+  public games: Array<Game>;
 
-  constructor() { }
+  constructor(private httpService: HttpService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      // Check if there is a search query for a particular game
+      if(params['game-search']){
+        this.searchGames('metacrit', params['game-search']);
+      }
+      else{
+        this.searchGames('metacrit');
+      }
+    })
+  }
+
+  // Searching games, waiting to get an observable of an APIResponse. Once received, set the results to games
+  searchGames(sort: string, search?: string) : void{
+    this.httpService
+      .getGamesList(sort, search)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList.results);
+      });
   }
 
 }
